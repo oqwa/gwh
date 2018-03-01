b325f2972b3a036fedd0165393feec7dfdb0175f
oqwa <oqwa@inbox.lv>
2018-03-01 18:42:27 +0300

    removed fixture from tests

--- tests/test_wildcard.py
+++ tests/test_wildcard.py
@@ -1,17 +1,11 @@
-import pytest
-
 from gwh import *
 from tests.utils import *
 
-
-@pytest.fixture
-def app():
-    app = GitWebhook()
-    app.add_handler(lambda: None)
-    return app
+app = GitWebhook()
+app.add_handler(lambda: None)
 
 
-def test_bitbucket(app):
+def test_bitbucket():
     check_bitbucket_webhook(app, KNOWN_TYPE,   KNOWN_REPO,   "master", hit_expected=True)
     check_bitbucket_webhook(app, UNKNOWN_TYPE, KNOWN_REPO,   "master", hit_expected=True)
     check_bitbucket_webhook(app, KNOWN_TYPE,   UNKNOWN_REPO, "master", hit_expected=True)
